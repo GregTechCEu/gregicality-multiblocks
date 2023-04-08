e90bbde22d0da2a2daebc2886fc9f7e0941bef81
Tech22 <37029404+TechLord22@users.noreply.github.com>
2023-04-07 22:30:38 -0400

    1.2.1

--- src/main/java/gregicality/multiblocks/GregicalityMultiblocks.java
+++ src/main/java/gregicality/multiblocks/GregicalityMultiblocks.java
@@ -18,7 +18,7 @@ public class GregicalityMultiblocks {
 
     public static final String MODID = "gcym";
     public static final String NAME = "Gregicality Multiblocks";
-    public static final String VERSION = "@VERSION@";
+    public static final String VERSION = "1.2.1";
 
     @SidedProxy(modId = MODID, clientSide = "gregicality.multiblocks.common.ClientProxy", serverSide = "gregicality.multiblocks.common.CommonProxy")
     public static CommonProxy proxy;
