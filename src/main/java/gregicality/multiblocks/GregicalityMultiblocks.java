91a0dbddc9ca139bd82f9542a8354b674b69bf05
alongstringofnumbers <sjensen793@gmail.com>
2023-05-07 20:44:34 -0700

    Begin RFG Port

--- src/main/java/gregicality/multiblocks/GregicalityMultiblocks.java
+++ src/main/java/gregicality/multiblocks/GregicalityMultiblocks.java
@@ -5,7 +5,7 @@ import gregicality.multiblocks.api.utils.GCYMLog;
 import gregicality.multiblocks.common.CommonProxy;
 import gregicality.multiblocks.common.block.GCYMMetaBlocks;
 import gregicality.multiblocks.common.metatileentities.GCYMMetaTileEntities;
-import gregtech.api.GTValues;
+import gregtech.GTInternalTags;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -13,7 +13,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 @Mod(   modid        = GregicalityMultiblocks.MODID,
         name         = GregicalityMultiblocks.NAME,
         version      = GregicalityMultiblocks.VERSION,
-        dependencies = GTValues.MOD_VERSION_DEP)
+        dependencies = GTInternalTags.DEP_VERSION_STRING)
 public class GregicalityMultiblocks {
 
     public static final String MODID = "gcym";
