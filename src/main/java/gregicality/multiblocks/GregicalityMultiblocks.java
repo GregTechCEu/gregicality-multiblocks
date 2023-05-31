89f722528cd055051aa0ec3109e2e4637276dbc4
DStrand1 <dane@strandboge.com>
2023-05-30 21:16:22 -0500

    fixes

--- src/main/java/gregicality/multiblocks/GregicalityMultiblocks.java
+++ src/main/java/gregicality/multiblocks/GregicalityMultiblocks.java
@@ -18,7 +18,7 @@ public class GregicalityMultiblocks {
 
     public static final String MODID = "gcym";
     public static final String NAME = "Gregicality Multiblocks";
-    public static final String VERSION = "1.2.2";
+    public static final String VERSION = GCYMInternalTags.VERSION;
 
     @SidedProxy(modId = MODID, clientSide = "gregicality.multiblocks.common.ClientProxy", serverSide = "gregicality.multiblocks.common.CommonProxy")
     public static CommonProxy proxy;
